9fc089e048606faf9ba9191f597a451d6f780178
two-six <twopsix@gmail.com>
2022-03-02 14:11:17 +0100

    Moderator stuff

--- routes/main.ts
+++ routes/main.ts
@@ -2,10 +2,12 @@ import index from './index.ts';
 import recipies from './recipies.ts';
 import user from './user.ts';
 import comments from './comments.ts';
+import mod from './mod.ts'
 
 export default [
   index,
   recipies,
   user,
-  comments
+  comments,
+  mod
 ];
